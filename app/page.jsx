c29423be8950aa5a0493656b6e1a7dca87fead48
Dkyadav10 <dishantyadav839@gmail.com>
2025-03-23 00:42:18 +0530

    1st commit

---
+++ app/page.jsx
@@ -0,0 +1,4 @@
+export default function Home() {
+  return <main className = "text-2xl">homepage</main>
+}
+  
---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+// import Link from "next/Link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+};
+
+export default Header;
