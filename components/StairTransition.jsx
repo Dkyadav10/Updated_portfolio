b667bbc09edbf3d8adb9df8ea58cced948fc3248
Dkyadav10 <dishantyadav839@gmail.com>
2025-04-03 22:13:31 +0530

    commit

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
-import { usePathname } from "next/navigation";
+import { usePathname } from "next/navigation"
 
-<<<<<<< HEAD
 //components
 import Stairs from "./Stairs"
 
-=======
->>>>>>> 453dca177580596773c96f07bd31613a5dc8bb7d
+
 const StairTransition = () => {
     const pathname = usePathname();
   return (
