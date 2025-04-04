97440671c1432c6b7ff546b7eb3f6233fdb668f2
Dkyadav10 <dishantyadav839@gmail.com>
2025-04-04 13:55:24 +0530

    before home commit

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -7,23 +7,23 @@ import { CiMenuFries } from "react-icons/ci";
 
 const links = [
     {
-        name : "home",
+        name : "Home",
         path : '/',
     },
     {
-        name : "services",
+        name : "Services",
         path : '/services',
     },
     {
-        name : "resume",
+        name : "Resume",
         path : '/resume',
     },
     {
-        name : "work",
+        name : "Work",
         path : '/work',
     },
     {
-        name : "contact",
+        name : "Contact",
         path : '/contact',
     },
    
@@ -39,10 +39,34 @@ const MobileNav = () => {
         </SheetTrigger>
 
         <SheetContent className = "flex flex-col">
+
             {/* logo */}
-            <div>
-                logo
+            <div className = "mt-32 mb-40 text-center text-2xl">
+                <Link href = "/">
+                    <h1 className = "text-4xl font-semibold ">
+                        Dharmendra Kumar Yadav <span className = "text-[#00ff99]"></span>
+                    </h1>
+                </Link>
+               
             </div>
+
+            {/* nav */}
+            <nav className = "flex flex-col justify-center items-center gap-8">
+                {links.map((link, index) => {
+                    return (
+                        <Link 
+                            href={link.path} 
+                            key={index} 
+                            className={`${
+                             link.path === pathname &&
+                                "text-[#00ff99] border-b-2 border-[#00ff99]"
+                                } text-xl capitalize hover:text-[#00ff99] transition-all`}
+                        >
+                            {link.name}
+                        </Link>
+                    );
+                })}
+            </nav>
         </SheetContent>
     </Sheet>
   );
