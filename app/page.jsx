b044b95823a41bfe441184c097a8117cdce49f12
Dkyadav10 <dishantyadav839@gmail.com>
2025-04-05 22:48:46 +0530

    home last commit

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
