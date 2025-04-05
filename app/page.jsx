587f020100442fb1409629c40a852b6b1ca45859
Dkyadav10 <dishantyadav839@gmail.com>
2025-04-05 21:59:40 +0530

    before stats

--- app/page.jsx
+++ app/page.jsx
@@ -1,51 +1,56 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
     <section className = "h-full">
-        <div className = "container mx-auto h-full">
-          <div className = "flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+      <div className = "container mx-auto h-full">
+        <div className = "flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
             
-            {/* Text */}
-            <div className = "text-center xl:text-left">
-              <span className = "text-xl">Full Stack Developer</span>
+          {/* Text */}
+          <div className = "text-center xl:text-left order-2 xl:order-none">
+            <span className = "text-xl">Full Stack Developer</span>
 
-              <h1 className = "h1 mb-6">
-                Hello I'm <br /> 
-                <span className = "text-[#00ff99]">Dharmendra Kumar Yadav</span>
-              </h1>
+            <h1 className = "h1 mb-6">
+              Hello I'm <br /> 
+              <span className = "text-[#00ff99]">Dharmendra Kumar Yadav</span>
+            </h1>
 
-              <p className = "max-w-[500px] mb-9 text-white/80">
-              I excel at crafting elegant digital experience and I am proficient in various programming language and technologies
-              </p>
+            <p className = "max-w-[500px] mb-9 text-white/80">
+            I excel at crafting elegant digital experience and I am proficient in various programming language and technologies
+            </p>
 
-              {/* Button and Socials */}
-              <div className = "flex flex-col xl:flex-row items-center gap-8">
-                <Button 
-                  variant = "outline"
-                  size = "lg"
-                  className = "uppercase flex items-center gap-2">
-                    <span>Download CV</span>
-                    <FiDownload />
-                </Button>
-                <div className = "mb-8 xl:mb-0">
-                  <Social />
-                </div>
+            {/* Button and Socials */}
+            <div className = "flex flex-col xl:flex-row items-center gap-8">
+              
+              <Button 
+                variant = "outline"
+                size = "lg"
+                className = "uppercase flex items-center gap-2"
+              >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social 
+                  containerStyles="flex gap-6" 
+                  iconStyles="w-9 h-9 border border-[#00ff99] rounded-full flex justify-center items-center text-[#00ff99] text-base hover:bg-[#00ff99] hover:text-[#1c1c22] hover:transition-all duration-500" 
+                />
               </div>
-
             </div>
-
-            {/* Photo */}
-            <div>
-              Photo
-            </div>
-
           </div>
+
+          {/* Photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+
         </div>
-    </section>
-    
+      </div>
+      <Stats />
+    </section> 
   );
 };
 
